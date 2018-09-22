c1d00a972e9425a97cca92e84c9631cdd6fd6577
gpDA <geonpy@likelion.org>
2018-09-22 10:17:02 -0400

    gitignore update

---
+++ project/frontend/handlingdata.py
@@ -0,0 +1,25 @@
+def dateConvert(date):
+    Rdata = ''
+    if "Mon":
+        Rdata = "Monday"
+    elif "Tue":
+        Rdata = "Tuesday"
+    elif "Wed":
+        Rdata = "Wednesday"
+    elif "Thu":
+        Rdata = "Thursday"
+    elif "Fri":
+        Rdata = "Friday"
+    elif "Sat":
+        Rdata = "Saturday"
+    elif "Sun":
+        Rdata = "Sunday"
+    else:
+        Rdata = "Undefined"
+    
+    return Rdata
+        
+        
+        
+        
+        
--- project/frontend/views.py
+++ project/frontend/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.shortcuts import render
 import tweepy
 from tweepy.auth import OAuthHandler
+from . import handlingdata as hd
 
 def TweetLive(request):
     auth = OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
@@ -14,6 +15,35 @@ def TweetLive(request):
     print(settings.ACCESS_TOKEN)
     print(settings.ACCESS_TOKEN_SECRET)
 
-    for status in tweepy.Cursor(api.search, q="#nyu", count="1", result_type="popular").items():
-        print(status)
+    #tweet_data = {}
+    for status in tweepy.Cursor(api.search, q="#NYU", count=0, result_type="popular").items():
+        json_data = (status._json)
+        
+        #popular_txt = json_data['text']
+        #user.name
+        #user.screen_name
+        #created_at
+        #retweet_count
+        #favorite_count
+        print('text',json_data['text'])
+        print('user.name',json_data['user']['name'])
+        print('user.name',json_data['user']['screen_name'])
+        #print('user.screen_name',json_data.user['screen_name'])
+        #print((json_data['created_at'].split(" ")))
+        dta = (json_data['created_at'].split(" "))
+        print(hd.dateConvert(dta[0])) #Thu
+        print(dta[1]) #Sep
+        print(dta[2]) #13 #day
+        print(dta[3][0:2]) #14 hour
+        print(dta[3][3:5]) #20 min
+        print(dta[5]) #2018
+
+        
+
+
+
+        print('retweet_count',json_data['retweet_count'])
+        print('favorite_count',json_data['favorite_count'])
+
+    
     return render(request, 'frontend/index.html') 
